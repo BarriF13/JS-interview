1e60499361142c4e9d0c1c32289709be267e2011
Barri <info@bdesignstudio.co.uk>
2020-09-17 18:11:47 +0100

    add simple recursive func

--- recursion.js
+++ recursion.js
@@ -1,20 +1,34 @@
 //Under standing callstack in developer tools
-function takeShower(){
-  return "Showering!"
-}
-function eatBreakfast(){
-  let meal = cookFood()
-  return `Eating ${meal}`
-}
-function cookFood(){
-  let items = ['pancake', 'Eggs', 'shakes']
-  return items[Math.floor(Math.random() * items.length)]; 
-}
+// function takeShower(){
+//   return "Showering!"
+// }
+// function eatBreakfast(){
+//   let meal = cookFood()
+//   return `Eating ${meal}`
+// }
+// function cookFood(){
+//   let items = ['pancake', 'Eggs', 'shakes']
+//   return items[Math.floor(Math.random() * items.length)]; 
+// }
 
-function wakeUp(){
-  takeShower();
-  eatBreakfast()
-  console.log(' Ok I am ready to Code now!')
-}
+// function wakeUp(){
+//   takeShower();
+//   eatBreakfast()
+//   console.log(' Ok I am ready to Code now!')
+// }
 
-wakeUp()
+// wakeUp()
+
+// -- simple recursion function
+
+function countDown(num){
+  if(num <=0){
+    console.log("All done");
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+  
+}
+countDown(3)
