12ab0e1024a0186b434c8fdfe58a3cb7bac3e3fe
Barri <info@bdesignstudio.co.uk>
2020-09-18 13:47:42 +0100

    pure recursion

--- recursion.js
+++ recursion.js
@@ -1,92 +1,108 @@
-//Under standing callstack in developer tools
-// function takeShower(){
-//   return "Showering!"
+// //Under standing callstack in developer tools
+// // function takeShower(){
+// //   return "Showering!"
+// // }
+// // function eatBreakfast(){
+// //   let meal = cookFood()
+// //   return `Eating ${meal}`
+// // }
+// // function cookFood(){
+// //   let items = ['pancake', 'Eggs', 'shakes']
+// //   return items[Math.floor(Math.random() * items.length)]; 
+// // }
+
+// // function wakeUp(){
+// //   takeShower();
+// //   eatBreakfast()
+// //   console.log(' Ok I am ready to Code now!')
+// // }
+
+// // wakeUp()
+
+// // -- simple recursion function
+
+// function countDown(num){
+//   if(num <=0){
+//     console.log("All done");
+//     return;
+//   }
+//   console.log(num);
+//   num--;
+//   countDown(num);
+
 // }
-// function eatBreakfast(){
-//   let meal = cookFood()
-//   return `Eating ${meal}`
-// }
-// function cookFood(){
-//   let items = ['pancake', 'Eggs', 'shakes']
-//   return items[Math.floor(Math.random() * items.length)]; 
+// countDown(3)
+
+// //--- different recursion
+
+// function sumRange(num){
+//   if(num === 1 )return 1;
+//   return num + sumRange(num-1);
 // }
 
-// function wakeUp(){
-//   takeShower();
-//   eatBreakfast()
-//   console.log(' Ok I am ready to Code now!')
+// console.log(sumRange(6))
+
+// // --- another basic recursion
+// //none recursive solution  
+// const factorial=(num)=>{
+//   let total =1; 
+//   for( i = num; i>0 ;i--){
+//     total *= i
+//   }
+//   return total
+// }
+// console.log(factorial(5))
+// //Where things go wrong
+// // No base case
+// // Forgetting to return or returning the wrong thing!
+// // Stack overflow! 
+// const factorialNum = (num1)=>{
+//   if(num1 === 1 )return 1;//  base case 
+//   return num1 * factorialNum(num1 -1)
 // }
 
-// wakeUp()
+// console.log(factorialNum(3))
 
-// -- simple recursion function
+// //--- Helper method  sample recursion -two functions -main outer and inside recursion
+// const outer = (input)=>{
+//   let outerScopedVar = []
 
-function countDown(num){
-  if(num <=0){
-    console.log("All done");
-    return;
+//   const helper =(helperInput)=>{
+//     //modify outerScopeVar
+//     helper(helperInput--)
+//   }
+//   helper(input)
+//   return outerScopedVar;
+// }
+// // an example -- collect odd value recursively 
+// const collectOdds =(arr)=>{
+//   let result = [];
+//     const helper=(helperInput)=>{
+//       if( helperInput.length === 0){
+//         return;
+//       }
+//       if(helperInput[0] %2 !== 0 ){
+//         result.push(helperInput[0])
+//       }
+//       helper(helperInput.slice(1)) // removing the first number then loop again 
+//     }
+//     helper(arr)
+//     return result;
+// }
+// console.log(collectOdds([1,2,3,4,5,6,7,8,9]));
+
+// //--- Pure recursion
+const collectOddsVal = (array) => {
+  let newArr = [];
+
+  if (array.length === 0) {
+    return newArr
   }
-  console.log(num);
-  num--;
-  countDown(num);
-  
-}
-countDown(3)
-
-//--- different recursion
-
-function sumRange(num){
-  if(num === 1 )return 1;
-  return num + sumRange(num-1);
-}
-
-console.log(sumRange(6))
-
-// --- another basic recursion
-//none recursive solution  
-const factorial=(num)=>{
-  let total =1; 
-  for( i = num; i>0 ;i--){
-    total *= i
+  if (array[0] % 2 !== 0) {
+    newArr.push(array[0])
   }
-  return total
-}
-console.log(factorial(5))
-//Where things go wrong
-// No base case
-// Forgetting to return or returning the wrong thing!
-// Stack overflow! 
-const factorialNum = (num1)=>{
-  if(num1 === 1 )return 1;//  base case 
-  return num1 * factorialNum(num1 -1)
+  newArr = newArr.concat(collectOddsVal(array.slice(1)));
+  return newArr;
 }
 
-console.log(factorialNum(3))
-
-//--- Helper method  sample recursion -two functions -main outer and inside recursion
-const outer = (input)=>{
-  let outerScopedVar = []
-
-  const helper =(helperInput)=>{
-    //modify outerScopeVar
-    helper(helperInput--)
-  }
-  helper(input)
-  return outerScopedVar;
-}
-// an example -- collect odd value recursively 
-const collectOdds =(arr)=>{
-  let result = [];
-    const helper=(helperInput)=>{
-      if( helperInput.length === 0){
-        return;
-      }
-      if(helperInput[0] %2 !== 0 ){
-        result.push(helperInput[0])
-      }
-      helper(helperInput.slice(1)) // removing the first number then loop again 
-    }
-    helper(arr)
-    return result;
-}
-console.log(collectOdds([1,2,3,4,5,6,7,8,9]));
+console.log(collectOddsVal([1, 2, 3, 4, 5]));
