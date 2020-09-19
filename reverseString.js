79730fa7b06ea681b3c168dda167c2cec5268907
Barri <info@bdesignstudio.co.uk>
2020-09-19 08:39:25 +0100

    reverse num

--- reverseString.js
+++ reverseString.js
@@ -3,3 +3,8 @@ const  reverseStr =(str) =>{
   return str.split('').reverse().join('')
 }
 console.log(reverseStr('hello'))
+// integer reverse
+const reverseInt = (int) =>{
+  return int.toString().split('').reverse().join('')
+}
+console.log(reverseInt(1234))
