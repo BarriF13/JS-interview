4155fe99cf31f767d0432dc42d84fb5328b66ac9
Barri <info@bdesignstudio.co.uk>
2020-09-21 16:05:38 +0100

    refactor and add helper function

--- countChar.js
+++ countChar.js
@@ -1,12 +1,23 @@
 // count character in string
+
+//charCodeAt() gives us laphabetical code 
+const isAlphaNumeric = (char) => {
+  let code = char.charCodeAt(0);
+  if (!(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)) {
+    return false;
+  }
+  return true;
+}
 const countChar = (str) => {
   let obj = {};
   for (char of str) {
-     char = char.toLowerCase();
-    if (/[a-z0-9]/.test(char)) {
-      obj[char] > 0 ?obj[char]++ : obj[char] = 1;
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] > 0 ? obj[char]++ : obj[char] = 1;
     }
   }
   return obj;
 }
-console.log(countChar('nnbezinn olof'))
+console.log(countChar('cyrus Cyber cu jon n'))
