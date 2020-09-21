03b2e3d0acc6d2e88396c1741b8d3240c6f5e26c
Barri <info@bdesignstudio.co.uk>
2020-09-21 15:56:42 +0100

    refactor

--- countChar.js
+++ countChar.js
@@ -1,14 +1,12 @@
 // count character in string
 const countChar = (str) => {
-  let result = {};
-  for(i in str ){
-    let char = str[i];
-    if(result[char] > 0) {
-      result[char]++;
-    } else {
-      result[char] = 1;
+  let obj = {};
+  for (char of str) {
+     char = char.toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      obj[char] > 0 ?obj[char]++ : obj[char] = 1;
     }
   }
-  return result;
+  return obj;
 }
-  console.log(countChar('zaghsdsart'))
+console.log(countChar('nnbezinn olof'))
